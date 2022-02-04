8fe0fcec03e6ebe655341371a070b236ffe9c6dc
Mike Conley <conleym@users.noreply.github.com>
2022-02-04 10:27:09 -0500

    Fix .gitconfig. Add more work shell stuff.

--- .profile.d/addons/work.sh
+++ .profile.d/addons/work.sh
@@ -3,6 +3,7 @@ export EVHOME=~/evhome
 export PATH="${EVHOME}/ev/bin:${PATH}"
 . "${EVHOME}/ev/shell/data.sh"
 . "${EVHOME}/ev/shell/postgresql.sh"
+. "${EVHOME}/ev/shell/autossh.sh"
 
 
 # Local Variables:
