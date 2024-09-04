85d22287067bbe9f0701362f533505040a3560d7
Mike Conley <conleym@users.noreply.github.com>
2024-09-04 14:38:02 -0400

    Remove autossh, which I no longer use.

--- .profile.d/addons/work.sh
+++ .profile.d/addons/work.sh
@@ -3,7 +3,6 @@ export EVHOME=~/evhome
 export PATH="${EVHOME}/ev/bin:${PATH}"
 . "${EVHOME}/ev/shell/data.sh"
 . "${EVHOME}/ev/shell/postgresql.sh"
-. "${EVHOME}/ev/shell/autossh.sh"
 
 
 # Local Variables:
