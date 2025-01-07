a5310609f74cfa1da036091a2fbd3ccfae23775c
Mike Conley <conleym@users.noreply.github.com>
2025-01-07 00:06:13 -0500

    Add emacs vars to end of work.sh.

--- .profile.d/addons/work.sh
+++ .profile.d/addons/work.sh
@@ -3,3 +3,10 @@ source ~/alloyhome/alloy/shell/postgres.sh
 if [ -x ~/bin/doppler ]; then
     source <(doppler completion 2> /dev/null)
 fi
+
+
+
+# Local Variables:
+# mode: sh
+# sh-shell: zsh
+# End:
