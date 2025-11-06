dc52e9a04463d270e4eb0cf6776565ef56e65fdf
Mike Conley <conleym@users.noreply.github.com>
2025-11-06 10:03:52 -0500

    Remove boto2 fallback in aws shell function.
    No need to support ancient boto2. The last release was in 2018. If
    boto3 isn't installed, too bad.

--- .profile.d/addons/aws.sh
+++ .profile.d/addons/aws.sh
@@ -4,20 +4,12 @@ _other_aws_common_stuff() {
     unset AWS_ACCESS_KEY_ID
     unset AWS_SECRET_ACCESS_KEY
     define SCRIPT <<EOF
-try:
-  # Prefer boto3
   from botocore.session import Session
   s = Session()
   c = s.get_credentials()
   if c:
      print('export AWS_ACCESS_KEY_ID=' + c.access_key)
      print('export AWS_SECRET_ACCESS_KEY=' + c.secret_key)
-except ImportError as e:
-  # Try boto if boto3 isn't available.
-  from boto.provider import Provider
-  p = Provider('aws')
-  print('export AWS_ACCESS_KEY_ID=' + p.get_access_key())
-  print('export AWS_SECRET_ACCESS_KEY=' + p.get_secret_key())
 EOF
     local RESULT
     RESULT=$(echo "${SCRIPT}" | /usr/bin/python3)
